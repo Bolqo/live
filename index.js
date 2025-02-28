80b308509f761a12699add49f0f64c19bcb7eb19
Bolqo <irakli.bolkvadze@outlook.com>
2025-02-28 20:11:13 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function ingredientsHover(){
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "300%";
+
+}
+function ingredientsNormal(){
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%";
+
+}
+function preparationHover(){
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "300%";
+
+}
+function preparationNormal(){
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "100%";
+
+}
